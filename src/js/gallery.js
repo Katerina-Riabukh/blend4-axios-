ad648a314525bb482f100c5b88e8759d323085ae
Janne57 <evgeniyahome1981@gmail.com>
2023-06-04 17:16:11 +0300

    finish

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,6 +1,8 @@
 import { UnsplashAPI } from './UnsplashAPI'
 import createGalleryCard from '../templates/gallery-card.hbs'
 import refs from './refs'
+import Pagination from 'tui-pagination'
+import 'tui-pagination/dist/tui-pagination.css'
 
 /**
   |============================
@@ -20,12 +22,21 @@ import refs from './refs'
   | Якщо у нас залишився час, давате підключимо перемикач теми. Він знаходиться у файлі "isChangeTheme.js".
   |============================
 */
+refs.form.addEventListener('submit', onSearchFormSubmit);
 
+const options = { 
+  totalItems: 5,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+  };
 
 const unsplashApi = new UnsplashAPI()
+const pagination = new Pagination(refs.container, options);
 
-const page = 1
+const page = pagination.getCurrentPage();
 onRenderPage(page)
+
 
 async function onRenderPage(page) {
  
@@ -33,9 +44,87 @@ async function onRenderPage(page) {
 
     const response = await unsplashApi.getPopularPhotos(page)
     refs.galleryList.innerHTML = createGalleryCard(response.data.results)
+    console.log(response.data.total);
+    refs.container.classList.remove('is-hidden');
 
+    
+    pagination.reset(response.data.total);
   } catch(error) {
     console.log(error);
    }
   
+}
+
+async function createPopularPagination(event) {
+  const currentPage = event.page;
+  console.log(currentPage);
+
+  try {
+
+    const response = await unsplashApi.getPopularPhotos(currentPage)
+    refs.galleryList.innerHTML = createGalleryCard(response.data.results)
+    console.log(response.data.total);
+    
+  } catch(error) {
+    console.log(error);
+   }
+}
+
+
+pagination.on('afterMove', createPopularPagination);
+
+
+async function onSearchFormSubmit(event){
+  event.preventDefault();
+  pagination.off('afterMove', createPopularPagination);
+
+  const searchQuery = event.currentTarget.elements['user-search-query'].value.trim();
+  console.log(searchQuery);
+  
+  unsplashApi.query = searchQuery;
+
+  if (!searchQuery) {
+    return alert('Error')
+  }
+
+  try {
+
+    const response = await unsplashApi.getPhotosByQuery(page)
+    refs.galleryList.innerHTML = '';
+
+    
+
+    refs.galleryList.innerHTML = createGalleryCard(response.data.results)
+    pagination.reset(response.data.total)
+    // console.log(response.data.total);
+    pagination.on('afterMove', createByQueryPagination);
+
+    if (response.data.total < 12) {
+      refs.container.classList.add('is-hidden');
+    } else {
+      refs.container.classList.remove('is-hidden');
+    }
+    
+    console.log(response.data.total);
+    
+  } catch(error) {
+    console.log(error);
+   }
+
+
+}
+
+async function createByQueryPagination(event) {
+  const currentPage = event.page;
+  console.log(currentPage);
+
+  try {
+
+    const response = await unsplashApi.getPhotosByQuery(currentPage)
+    refs.galleryList.innerHTML = createGalleryCard(response.data.results)
+    console.log(response.data.total);
+    
+  } catch(error) {
+    console.log(error);
+   }
 }
